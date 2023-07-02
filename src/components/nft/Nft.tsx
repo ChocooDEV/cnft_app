70366a2acce7f737c2fa45dc620913048f001c0c
ChocooDEV <chocoo.web3@gmail.com>
2023-07-02 16:44:36 +0200

    Added unique Card selection + highlight

--- src/components/nft/Nft.tsx
+++ src/components/nft/Nft.tsx
@@ -5,40 +5,40 @@ import Card from "./Card";
 const url = `https://mainnet.helius-rpc.com/?api-key=b941213c-6a24-44da-b1ec-5dda9ab09e4d`
 const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
-
 const getMetadata = async (mintAccounts: string[]) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            mintAccounts,
-            includeOffChain: true,
-            disableCache: false,
-        }),
-    });
-      
-    const data = await response.json();
-    return data;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      mintAccounts,
+      includeOffChain: true,
+      disableCache: false,
+    }),
+  });
+
+  const data = await response.json();
+  return data;
 };
 
 const searchAssets = async (ownerAddress: string) => {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "my-id",
+      method: "searchAssets",
+      params: {
+        ownerAddress,
+        page: 1,
       },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        id: "my-id",
-        method: "searchAssets",
-        params: {
-          ownerAddress,
-          page: 1,
-        },
-      }),
-    });
+    }),
+  });
+
   const { result } = await response.json();
   const groupedResults: {
     id: string;
@@ -52,9 +52,9 @@ const searchAssets = async (ownerAddress: string) => {
       json_uri: result.items[i].content.json_uri,
       compressed: result.items[i].compression.compressed,
       attributes: result.items[i].content?.metadata?.attributes?.map(
-        ({ traitType, trait_type, value}) => ({
-            traitType: traitType || trait_type,
-            value,
+        ({ traitType, trait_type, value }) => ({
+          traitType: traitType || trait_type,
+          value,
         })
       ) || []
     };
@@ -77,57 +77,64 @@ const searchAssets = async (ownerAddress: string) => {
 };
 
 export const Nft = () => {
-    const wallet = useWallet();
-    const [data, setData] = useState<any>({ owner: "", assets: [] });
-  
-    useEffect(() => {
-      if (wallet.connected && wallet.publicKey) {
-        const ownerAddress = wallet.publicKey.toBase58();
-        searchAssets(ownerAddress)
-          .then(async (groupedResults) => {
-            const updatedAssets = [];
-  
-            for (const group of groupedResults) {
-              const assetsWithMetadata = await Promise.all(
-                group.assets.map(async (asset) => {
-                  const metadataResponse = await fetch(asset.json_uri);
-                  const metadata = await metadataResponse.json();
-  
-                  return {
-                    ...asset,
-                    metadata,
-                  };
-                })
-              );
-  
-              updatedAssets.push({
-                ...group,
-                assets: assetsWithMetadata,
-              });
-            }
-  
-            setData({ owner: ownerAddress, assets: updatedAssets });
-          })
-          .catch((error) => {
-            console.error("Error fetching assets:", error);
-          });
-      }
-    }, [wallet]);
-  
-    return (
-        <div className="p-4">
-          <h1>gm! {data.owner} Here are your NFTs</h1>
-          <div className="card-grid">
-            {data.assets.map((group: any) =>
-              group.assets.map((nft: any) => (
-                <div key={nft.id} className="card-grid-item">
-                  <Card nft={nft} />
-                </div>
-              ))
-            )}
-          </div>
-        </div>
-      );
-      
+  const wallet = useWallet();
+  const [data, setData] = useState<any>({ owner: "", assets: [] });
+  const [selectedCardId, setSelectedCardId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (wallet.connected && wallet.publicKey) {
+      const ownerAddress = wallet.publicKey.toBase58();
+      searchAssets(ownerAddress)
+        .then(async (groupedResults) => {
+          const updatedAssets = [];
+
+          for (const group of groupedResults) {
+            const assetsWithMetadata = await Promise.all(
+              group.assets.map(async (asset) => {
+                const metadataResponse = await fetch(asset.json_uri);
+                const metadata = await metadataResponse.json();
+
+                return {
+                  ...asset,
+                  metadata,
+                };
+              })
+            );
+
+            updatedAssets.push({
+              ...group,
+              assets: assetsWithMetadata,
+            });
+          }
+
+          setData({ owner: ownerAddress, assets: updatedAssets });
+        })
+        .catch((error) => {
+          console.error("Error fetching assets:", error);
+        });
+    }
+  }, [wallet]);
+
+  const handleCardClick = (cardId: string) => {
+    setSelectedCardId(cardId === selectedCardId ? null : cardId);
   };
-  
+
+  return (
+    <div className="p-4">
+      <h1>gm! {data.owner} Here are your NFTs</h1>
+      <div className="card-grid">
+        {data.assets.map((group: any) =>
+          group.assets.map((nft: any) => (
+            <div
+              key={nft.id}
+              className={`card-grid-item ${selectedCardId === nft.id ? "selected" : ""}`}
+              onClick={() => handleCardClick(nft.id)}
+            >
+              <Card nft={nft} />
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
